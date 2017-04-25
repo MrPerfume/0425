ea4acca5631efcffa18eefa075d5e2fabea7020d
MrHuang <hoangluckyboy@icloud.com>
2017-04-25 22:26:51 +0800

    tp再玩玩

---
+++ shop/Home/Controller/UserController.class.php
@@ -0,0 +1,14 @@
+<?php
+namespace Home\Controller;
+use Think\Controller;
+class UserController extends Controller {
+    function login(){
+
+        echo 'login';
+
+    }
+
+    function register(){
+        echo 'zhuce';
+    }
+}
---
+++ shop/index.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 83571
+ * Date: 2017/4/25
+ * Time: 21:34
+ */
+//引入php框架
+include '../ThinkPHP/ThinkPHP.php';
